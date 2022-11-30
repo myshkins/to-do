c62e7b333c76729a6499d71f86ed2a64807c811b
mrpoopy <junk@krenitsky.org>
2022-11-30 14:13:40 -0500

    start setup

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+const todo = (title, description, priority) => {
+  const todoTitle = title;
+  const todoDesc = description;
+  const todoPriority = priority;
+
+  return {
+    todoTitle, todoDesc, todoPriority,
+  }
+}
+
+export default todo
